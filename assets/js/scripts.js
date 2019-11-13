aba6273012b12b5762fe90eac44e5ea42f594dfe
vanhoa1705 <17th.may.1999@gmail.com>
2019-11-13 19:13:01 +0700

    Change from bootstrap 3.0 to 4.0

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,16 +1,36 @@
+
+function scroll_to(clicked_link, nav_height) {
+	var element_class = clicked_link.attr('href').replace('#', '.');
+	var scroll_to = 0;
+	if(element_class != '.top-content') {
+		element_class += '-container';
+		scroll_to = $(element_class).offset().top - nav_height;
+	}
+	if($(window).scrollTop() != scroll_to) {
+		$('html, body').stop().animate({scrollTop: scroll_to}, 1000);
+	}
+}
+
+
 jQuery(document).ready(function() {
+	
+	/*
+	    Navigation
+	*/
+	$('a.scroll-link').on('click', function(e) {
+		e.preventDefault();
+		scroll_to($(this), $('nav').outerHeight());
+	});
+	// toggle "navbar-no-bg" class
+	$('.top-content .text').waypoint(function() {
+		$('nav').toggleClass('navbar-no-bg');
+	});
 	
     /*
         Background slideshow
     */
     $('.top-content').backstretch("assets/img/backgrounds/1.jpg");
-    
-    $('#top-navbar-1').on('shown.bs.collapse', function(){
-    	$('.top-content').backstretch("resize");
-    });
-    $('#top-navbar-1').on('hidden.bs.collapse', function(){
-    	$('.top-content').backstretch("resize");
-    });
+    $('.section-4-container').backstretch("assets/img/backgrounds/1.jpg");
     
     /*
         Wow
